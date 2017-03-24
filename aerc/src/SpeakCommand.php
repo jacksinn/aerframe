240e2ab139f62f6b720962f5959de9007d8bda0a
Steven <steven@rootedlogics.com>
2017-03-24 15:16:39 -0400

    console command for learning, want to add package generation etc

---
+++ aerc/src/SpeakCommand.php
@@ -0,0 +1,18 @@
+<?php
+namespace Aerc;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SpeakCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('aer:speak')->setDescription('Speak Message');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        exec('echo "Hello World"');
+    }
+}
