a7572e6b5504b2e6e0de790aa93be42e1fcaf52d
Steven <steven@rootedlogics.com>
2017-03-25 18:14:41 -0400

    Default

--- Aer/Core/View/View.php
+++ Aer/Core/View/View.php
@@ -9,6 +9,7 @@ class View {
     $path_arr = explode("/", debug_backtrace()[0]['file']);
     $calling_contoller = array_pop($path_arr);
 
+    //@todo im not sure this is how i should be doing this
     $template_path = implode("/", $path_arr);
     $template_path .= "/Theme/$template";
     ob_start();
