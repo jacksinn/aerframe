a6891153452278ff99c8943d2c5a6b97f376cdd4
Steven <steven@rootedlogics.com>
2017-03-25 10:52:39 -0400

    migration list command

---
+++ aerc/src/MigrateListCommand.php
@@ -0,0 +1,18 @@
+<?php
+namespace Aerc;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MigrateListCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('migrate:list')->setDescription('Lists Migrations');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        exec('echo "Listing migrations..."');
+    }
+}
