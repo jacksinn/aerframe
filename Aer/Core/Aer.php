b155698c0ba4654cffcaf69eb5aaa142c727e675
Steven <steven@rootedlogics.com>
2017-03-25 17:37:07 -0400

    Default

--- Aer/Core/Aer.php
+++ Aer/Core/Aer.php
@@ -7,19 +7,19 @@ use Aer\Database\MysqlConnection;
 class Aer
 {
     public static $config_path = __DIR__ . "/../../App/Config/config.json";
+
     public static function init()
     {
+        //@todo why was i doing a $conn here?
         $conn = MysqlConnection::connect();
-//        print_r($conn->query("SELECT * FROM users"));
 
-//        print_r(MysqlConnection::connect());
         if (!file_exists(self::$config_path)) {
             return "Serving Aer. Please setup the config file.";
         }
     }
 
     //@todo return json or null, other methods should handle the response, not her. smells.
-    public static function GetConfigurationOptions()
+    public static function GetConfigurationOptions(): ?string
     {
         if (!file_exists(self::$config_path)) {
             header('Location: /install.php');
