0f144b179029e1d91275620a6d093ecc3a46d278
Steven <steven@rootedlogics.com>
2017-04-02 10:46:50 -0400

    comments and questions

--- Aer/Core/Aer.php
+++ Aer/Core/Aer.php
@@ -6,13 +6,19 @@ use Aer\Database\MysqlConnection;
 
 class Aer
 {
+    //Where our configuration file lives
+    //@todo - combine this with .env?
     public static $config_path = __DIR__ . "/../../App/Config/config.json";
 
     public static function init()
     {
+        //Setting up db conn - does this mean I can avoid using it in
+        // other function calls?
         //@todo why was i doing a $conn here? to initialize?
         $conn = MysqlConnection::connect();
 
+        //Config file not setup, send message
+        //@todo maybe throw exception? Should I except all the things?
         if (!file_exists(self::$config_path)) {
             return "Serving Aer. Please setup the config file.";
         }
@@ -21,9 +27,14 @@ class Aer
     //@todo return json or null, other methods should handle the response, not her. smells.
     public static function GetConfigurationOptions()
     {
+        //Ok so I'm handling this up above in init and here - I
+        // shouldn't have multiple places doing the same check or at
+        //  the very least I should standardize it
         if (!file_exists(self::$config_path)) {
             header('Location: /install.php');
         } else {
+            //Decoding the json in the config file
+            //@todo return type is mixed, I'd like to guarantee what gets returned
             $config_json = json_decode(file_get_contents(self::$config_path));
             return $config_json;
         }
